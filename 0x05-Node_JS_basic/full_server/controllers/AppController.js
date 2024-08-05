6ab755f6a2fcd87d1ccd8efba3fc7f583b15e960
Garret <garretakpale@gmail.com>
2024-08-05 12:26:34 +0000

    task solution

---
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -0,0 +1,11 @@
+/**
+ * Creates Appcontroller class.
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
