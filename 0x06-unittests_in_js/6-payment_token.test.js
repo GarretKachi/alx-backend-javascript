dc7bab82084d3bcde0b53121a71091a7c45c9091
Garret <garretakpale@gmail.com>
2024-08-11 12:55:36 +0000

    payment

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,23 @@
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise with the correct data when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then(response => {
+        expect(response).to.deep.equal({ data: 'Successful response from the API' });
+        done();
+      })
+      .catch(error => done(error));
+  });
+
+  it('should do nothing when success is false', function(done) {
+    getPaymentTokenFromAPI(false)
+      .then(response => {
+        expect(response).to.be.undefined;
+        done();
+      })
+      .catch(error => done(error));
+  });
+});
+
